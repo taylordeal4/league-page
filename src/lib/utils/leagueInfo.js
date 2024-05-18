57b775a20415fe3474514779ceb0f17372d7094d
taylordeal4 <167900849+taylordeal4@users.noreply.github.com>
2024-05-18 14:52:18 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to A League of Our Own Fantasy Football League! Test</p>
+  <p>Welcome to A League of Our Own Fantasy Football League!</p>
 `;
 
 /*   STEP 3   */
