5dd70952794e8bab5b742175a6f58b3c78970967
taylordeal4 <167900849+taylordeal4@users.noreply.github.com>
2024-05-18 14:19:06 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -20,28 +20,28 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-    {
-      "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "Taylor",
-      "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "null", // (optional)
-      "bio": "Lorem ipsum...",
-      "photo": "/managers/Taylor.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "null", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "null", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
-        name: "Jerry", // Can be anything (usually your rival's name)
-        link: null, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/Jerry.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
-      "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "null", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Embrace the Chaos. Like a botched snap turned into a game-winning scramble, I thrive in the chaos of fantasy football. Unexpected injuries? Last-minute trades? Bring it on! We'll turn chaos into championships faster than you can say "Hail Mary".",
-      "tradingScale": 10, // 1 - 10
-      "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
+ //   {
+ //     "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+ //     "name": "Taylor",
+ //     "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+ //     "location": "null", // (optional)
+ //     "bio": "Lorem ipsum...",
+ //     "photo": "/managers/Taylor.jpg", // square ratio recommended (no larger than 500x500)
+ //     "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
+ //     "favoriteTeam": "null", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+ //     "mode": "null", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+ //     "rival": {
+ //       name: "Jerry", // Can be anything (usually your rival's name)
+ //       link: null, // manager array number within this array, or null to link back to all managers page
+ //       image: "/managers/Jerry.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+ //     },
+ //     "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+ //     "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+ //     "rookieOrVets": "null", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+ //     "philosophy": "Embrace the Chaos. Like a botched snap turned into a game-winning scramble, I thrive in the chaos of fantasy football. Unexpected injuries? Last-minute trades? Bring it on! We'll turn chaos into championships faster than you can say "Hail Mary".",
+ //     "tradingScale": 10, // 1 - 10
+ //     "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+ //   },
   ]
   
   
