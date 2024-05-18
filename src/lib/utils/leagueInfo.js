62bfc49c38a564f17f920e68256c06a246f22e84
taylordeal4 <167900849+taylordeal4@users.noreply.github.com>
2024-05-18 16:08:54 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,7 @@ export const managers = [
       "bio": "Commissioner.",
       "photo": "/managers/Taylor.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2012, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": det, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Jerry", // Can be anything (usually your rival's name)
