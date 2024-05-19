d00ed59a09acac13ac46a8827e0cc9dbf3961176
taylordeal4 <167900849+taylordeal4@users.noreply.github.com>
2024-05-18 23:16:41 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1086848843544674304"; // your league ID
 export const leagueName = "A League of Our Own"; // your league name
 export const dues = 75; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
