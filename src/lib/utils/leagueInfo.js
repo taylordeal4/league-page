db81be50e5ddf7c2d5e57c23716482647e973189
taylordeal4 <167900849+taylordeal4@users.noreply.github.com>
2024-05-18 21:47:42 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,8 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to "A League of Our Own" Fantasy Football League!</p>
-  <p>Where Fantasy Football Meets Modern Marvels</p>
+  <p>Welcome to "A League of Our Own" Fantasy Football League! Where Fantasy Football Meets Modern Marvels.</p>
   <p>Greetings gridirion gurus, welcome to the coolest fantasy football league in cyberspace - "A League of Our Own"! Get ready to kick off a season of digital domincation with  modern twist. </p>
   <p>Why Choose "A League of Our Own"?</p>
   <p>-Slick Interface, Smooth Sailing: Say goodbye to clunky interfaces and hello to sleek design. Our leagues' website, podcast, accompanied by the Sleeper app are as smooth as a perfect spiral, ensuring an effortless experience from draft day to championship glory.</p>
